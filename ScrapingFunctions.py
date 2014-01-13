daf938a71863acfd4d7db511daace8431a14f2ee
bdoyle91 <doyle.bren@ccs.neu.edu>
2014-01-13 16:33:35 -0500

    Update ScrapingFunctions.py
    Adapted code to use different stat pages, and to pull data from all of the pages we need at once.

--- ScrapingFunctions.py
+++ ScrapingFunctions.py
@@ -15,9 +15,9 @@ DIRECTORY = os.getcwd()+'/SoupyFootballData.html'
 #                                                                                                
 #   **  exportToFile    **                                                                                     
 #  
-# Arguments: HTML ESPN-GO Stat Page               
-# Function: Downloads HTML source data and transfers data into a HTML file   
-# Returns: None
+# Arguments: 	HTML ESPN-GO Stat Page               
+# Function: 	Downloads HTML source data and transfers data into a HTML file   
+# Returns: 		None
 # 
 #                                                                                                  
 ##########################################################################################
@@ -32,24 +32,71 @@ def exportToFile(statPage):
     # Write soup to file                                                                                    
     f.write(str(soup))
 
-
-
-
 ##########################################################################################
 #
-# ** findPosition **
+# ** discoverStatTypes **
 #
-# Arguments: "Player Name, Position" string
-# Function: cuts final two characters off of string
-# Returns: Player's position
+# Arguments: 	List of String from HTML-GO Stat Table
+# Function: 	Finds out stat typees listed on page and removes them
+# Returns: 		List of string, Stat Types
 #
 #
 ##########################################################################################
 
-def findPosition(player):
-	splitp = player.split()
-	pos = splitp[len(splitp)-1]
-	print pos
+def discoverStatTypes(playerData, firstPlayer, statType=""):
+	#Record Stat Types in a list until we reach the first player in the table
+	type_List = []
+	for item in playerData:
+		if str(item) == firstPlayer:
+			break
+		if item!="TOUCHDOWNS" or item!="SCORING" and statType!="MiscScorers":
+			type_List.append(item)
+	if statType=="MiscScorers":
+		del type_List[0]
+		del type_List[0]
+	return type_List
+
+##########################################################################################
+#
+# ** removeStatTypes **
+#
+# Arguments: 	List of String from HTML-GO Stat Table
+# Function: 	Finds out stat types listed on page and removes them
+# Returns: 		None
+#
+#
+##########################################################################################
+
+def removeStatTypes(playerData, typeList):
+	#Remove each stat-type from the list
+	for item in typeList:
+		while item in playerData: playerData.remove(item)
+	while "TOUCHDOWNS" in playerData: playerData.remove("TOUCHDOWNS")
+	while "SCORING" in playerData: playerData.remove("SCORING")
+
+##########################################################################################
+#
+# ** separatePlayerPosition **
+#
+# Arguments:	List of String from HTML-GO Stat Table, location of player name in table
+#				structure, typeList Must be done BEFORE addition of POS to typelist
+# Function: 	Separates Player Name from Position
+# Returns: 		None
+#
+#
+##########################################################################################
+
+def separatePlayerPosition(playerData, typeList, position):
+	numberOfTypes = len(typeList)
+	numberOfPlayers = len(playerData)/numberOfTypes
+	for i in range(0, numberOfPlayers):
+		#i+position = original offset plus insertion offset
+		#i*numberOfTypes = space each player has taken up thus far
+		player = playerData[i+position+(i*numberOfTypes)]
+		#Split position and name, change name/position field to just name, insert position
+		player = player.rsplit(",")
+		playerData[i+position+(i*numberOfTypes)]=player[1]
+		playerData.insert(i+position+(i*numberOfTypes),player[0])
 
 
 
@@ -57,37 +104,73 @@ def findPosition(player):
 #
 # ** parseToString **
 #
-# Arguments: HTML ESPN-GO Stat Page
-# Function: Parses text from HTML file into a list of string
-# Returns: Player Data
+# Arguments: 	HTML ESPN-GO Stat Page
+# Function: 	Parses text from HTML file into a list of string
+# Returns: 		Player Data
 #
 #
 ##########################################################################################
 
 def parseToString(statPage):
 	exportToFile(statPage)
+
     # Replace first argument to open() with whatever file holds the html source code
 	h = open(DIRECTORY, 'r+')
 	s = h.read()
 	h.close()
-    # Turns string s into a BeautifulSoup object
+	# Turns string s into a BeautifulSoup object
 	soup = BeautifulSoup(s)
+
     # Searches the html for the tbody tag, which contains the entire table
-	playerList = soup.find_all("td", class_= "playertablePlayerName")
-	tableList = soup.find_all("td", class_="playertableStat")
-	x = 0
-	del tableList[0:15]
-    # Creates a list of everything
-	contents_list = []
-	playerContents = []
-	stringContents = []
-	players = []
-	for item in playerList:
-		playerContents.append(item.get_text())
-	for eachItem in tableList:
-		stringContents.append(eachItem.get_text())
-	qbs = []
-	while(len(playerContents)>0):
-		temp = playerContents.pop(0) + "," + stringContents.pop(0) + "," + stringContents.pop(0) + "," + stringContents.pop(0) + "," + stringContents.pop(0) + "," + stringContents.pop(0) + "," + stringContents.pop(0)+ "," + stringContents.pop(0)+ "," + stringContents.pop(0)+ "," + stringContents.pop(0)+ "," + stringContents.pop(0)+ "," + stringContents.pop(0)+ "," + stringContents.pop(0)+ "," + stringContents.pop(0)+ "," + stringContents.pop(0)+ "," + stringContents.pop(0)
-		players.append(temp)
-	return players
+	tbody_tag = soup.tbody
+	try:
+		td_list = tbody_tag.find_all_next("td")
+	    # Creates a list of everything
+		contents_list = []
+		#td_list = td_list.find_all(text = re.compile('&nbsp;'))
+		#for comment in findtoure:
+		#    fixed_text = unicode(comment).replace('&nbsp;', 'TEST TEST TEST')
+		#    comment.replace_with(fixed_text)
+		for tds in td_list:
+			contents_list.append(tds.get_text())
+		stringContents = []
+		for eachItem in contents_list:
+			stringContents.append(eachItem.encode('utf-8'))
+		return stringContents
+	except Exception, e:
+		return ""
+
+##########################################################################################
+#
+# ** getAllPages **
+#
+# Arguments: 	Default setup of a ESPN-GO stat page
+# Function: 	Parses text from HTML file into a list of string
+# Returns: 		Player Data
+#
+#
+##########################################################################################
+
+def getAllPages(statPage, statType=""):
+	count = 0
+	allPlayers = []
+	while True:
+		players = parseToString(statPage+str(1+count*40))
+		#Passers = scrapingFunctions.parseToString(Passing_Page)
+		if not players:
+			break
+
+		#Remove the state type information sprinkled in the list and store in secondary list
+		statTypeList = discoverStatTypes(players, str(1+(count*40)), statType)
+		removeStatTypes(players, statTypeList)
+
+		if statType=="MiscScorers":
+			del players[0]
+		separatePlayerPosition(players, statTypeList, 1)
+
+		#Insert the POS into our stat type list
+		statTypeList.insert(1, "POS")
+		count = count + 1
+		allPlayers += players
+	print(statTypeList)
+	return allPlayers
