a3852a5c28fd50f58ac2dce87528e5b89d7639e8
danabeled <danabeled@gmail.com>
2014-02-03 20:04:01 -0500

    Added in comments to teamClass and testBencher
    Also began implementation of testBencher

--- teamClass.py
+++ teamClass.py
@@ -1,3 +1,20 @@
+##########################################################################################
+#
+# CLASS: Team
+#
+# Members: 		List of class "Player"
+#						
+# Functions:	addPlayer - 		Adds a Player to the team,
+#						args:		Player x
+#						returns:	None
+#				getTotalPoints - 	Returns total number of points earned by
+#									players, does not yet account for weekly collisions
+#									or position collisions
+#						args:		None
+#						returns:	Points (integer)
+#
+##########################################################################################
+
 class Team:
 	def __init__(self):
 		self.Players = []
@@ -8,6 +25,17 @@ class Team:
 		for player in self.Players:
 			points = points + player.fantasyPoints
 		return points
+
+##########################################################################################
+#
+# CLASS: Player
+#
+# Members: 		name 	- First and Last Name of People
+#				pos 	- Position of Player
+#				points 	- total number of fantasy points earned by the player
+#
+#
+##########################################################################################
 
 class Player:
 	def __init__(self, fullName, position, points):
--- testBencher.py
+++ testBencher.py
@@ -1,1 +1,15 @@
-# To be implemented
+##########################################################################################
+#
+# ** benchMark **
+#
+# Arguments: 	List of player names and year to be evaluated
+# Function: 	Stores all the players in a Team class and then evalutes
+#				using built in function
+# Returns: 		Tean Fantasy Points
+#
+#
+##########################################################################################
+
+def benchMark(players, year="2013"):
+	points = 0 
+	return points
