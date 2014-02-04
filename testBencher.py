6c173bf634b4722e4289939b758c7dd9430f6ed5
danabeled <danabeled@gmail.com>
2014-02-04 12:41:02 -0500

    Added code to benchMark
    Also created unit test. benchMark cannot currently look up player
    position. Position to be added to fantasy tables so it can be easily
    looked up.

--- testBencher.py
+++ testBencher.py
@@ -1,3 +1,6 @@
+import teamClass
+import sqlite3 as lite
+
 ##########################################################################################
 #
 # ** benchMark **
@@ -11,5 +14,32 @@
 ##########################################################################################
 
 def benchMark(players, year="2013"):
-	points = 0 
+	conn = lite.connect('ESPN.db')
+	benchTeam = teamClass.Team()
+	for player in players:
+		c = conn.cursor()
+		command = "SELECT Player,Points FROM FantasyPoints_" + str(year) + " WHERE Player='" + str(player) + "'"
+		c.execute(command)
+		data = c.fetchall()
+		newPlayer = teamClass.Player(data[0][0],int(data[0][1]))
+		benchTeam.addPlayer(newPlayer)
+	conn.close()
+	points = benchTeam.getTotalPoints()
 	return points
+
+
+#########################################################################################
+#	Testing Teams Consisting of Following:
+#	1 QB
+#	2 RB
+#	2 WR
+#	1 TE
+#	1 FLEX
+#	1 DEF 	< - We don't have stats yet cannot implement
+#	1 K		< - We don't have stats yet cannot implement
+######### UNIT TEST MAIN ################################################################
+
+Players = ["Peyton Manning","Ray Rice","Reggie Bush","Dez Bryant","Greg Jennings","Jason Witten","Rashard Mendenhall"]
+benchPoints = benchMark(Players)
+print benchPoints
+
