0c2a9ef18c270400a9878927440c7aa8e47a84bb
danabeled <danabeled@gmail.com>
2014-03-30 19:10:10 -0400

    Created function to call SQL Commands

--- MostFantasyPointsAlgorithm.py
+++ MostFantasyPointsAlgorithm.py
@@ -10,3 +10,21 @@ MAX_DSTS_PER_TEAM = 3
 MAX_KS_PER_TEAM = 3
 
 TEAM_SIZE = 16
+
+##########################################################################################
+#
+# GLOBAL_FUNCTION: CALL_SQL_COMMAND
+#		
+# ARGS:			Database Name, Command String
+# Returns:		Returns SQL Cursor
+#
+##########################################################################################
+
+def CALL_SQL_COMMAND(database, command):
+	conn = lite.connect(database)
+	c = conn.cursor()
+	c.execute(command)
+	conn.close()
+	return c
+
+CALL_SQL_COMMAND("ESPN.db", "SELECT Player, Pos, Points FROM DraftList_2013")
