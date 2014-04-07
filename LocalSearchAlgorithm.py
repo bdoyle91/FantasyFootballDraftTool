41275323f1827fb8b07431541a28087185181a6d
danabeled <danabeled@gmail.com>
2014-04-07 17:07:14 -0400

    Created LocalSearch algorithm as subclass of greedy implementation

--- LocalSearchAlgorithm.py
+++ LocalSearchAlgorithm.py
@@ -12,32 +12,44 @@ from algorithmTester import *
 #
 ##########################################################################################
 
-class localSearchAlgorithm(Algorithm):
+class LocalSearchAlgorithm(GreedyByPositionAlgorithm):
 	def __init__(self):
 		self.name = ""
 		self.team = Team()
+		self.filledPositions = []
+		self.maxedPositions = []
 		self.draftRound = 1
 		self.draftSelectionsBeforeSearch = []
 
 	def __init__(self, inputName):
-		self.name = str(inputName)
-		self.team = Team()		
+		self.name = ""
+		self.team = Team()
+		self.filledPositions = []
+		self.maxedPositions = []
 		self.draftRound = 1
 		self.draftSelectionsBeforeSearch = []
-
-	def addNextPlayer(self, year):	
-		sqlHandler = SQL_HANDLER()
-		data = sqlHandler.CALL_SQL_SELECT("ESPN.db","Player, Pos, Points", "DraftList_"+str(year),"WHERE WasSelected=\'0\' ORDER BY Points DESC LIMIT \'1\'")
-		newPlayer = Player(data[0][0], int(data[0][2]), data[0][1])
-		self.team.addPlayer(newPlayer)
-		self.updateList(year, newPlayer)
-		self.draftRound = self.draftRound + 1
-		self.saveDraftSelections(year)
 
 	def saveDraftSelections(self, year):
 		sqlHandler = SQL_HANDLER()
 		data = sqlHandler.CALL_SQL_SELECT("ESPN.db","Player, WasSelected", "DraftList_"+str(year))
-		self.draftSelectionsBeforeSearch = data
+		self.draftSelectionsBeforeSearch = dat
+
+	def chooseNextPlayer(self, year):
+		sqlHandler = SQL_HANDLER()
+		if len(self.filledPositions) != 0 and len(self.filledPositions) < 6:
+			excludedPositions = self.generateStarterDraftString()
+			data = sqlHandler.CALL_SQL_SELECT("ESPN.db","Player, Pos, Points", "DraftList_"+str(year),"WHERE WasSelected=\'0\' AND Pos!=" + excludedPositions + " ORDER BY Points DESC LIMIT \'1\'")
+			self.checkFilledPositions()
+		elif len(self.maxedPositions)==0:
+			data = sqlHandler.CALL_SQL_SELECT("ESPN.db","Player, Pos, Points", "DraftList_"+str(year),"WHERE WasSelected=\'0\' ORDER BY Points DESC LIMIT \'1\'")
+			self.checkFilledPositions()
+			self.checkMaxedPositions()
+		else:
+			excludedPositions = self.generateMaxedDraftString()
+			data = sqlHandler.CALL_SQL_SELECT("ESPN.db","Player, Pos, Points", "DraftList_"+str(year),"WHERE WasSelected=\'0\' AND Pos!=" + excludedPositions + " ORDER BY Points DESC LIMIT \'1\'")
+			self.checkFilledPositions()
+			self.checkMaxedPositions()
+		return data
 
 
 
--- TestRun.py
+++ TestRun.py
@@ -3,13 +3,15 @@ from LocalSearchAlgorithm import *
 import time
 
 startTime = time.time()
-algoTester = algorithmTester([GreedyByPositionAlgorithm(1), GreedyByPositionAlgorithm(2), GreedyByPositionAlgorithm(3), GreedyByPositionAlgorithm(4), GreedyByPositionAlgorithm(5), GreedyByPositionAlgorithm(6), GreedyByPositionAlgorithm(7), GreedyByPositionAlgorithm(8), GreedyByPositionAlgorithm(9), GreedyByPositionAlgorithm(10)])
+algoTester = algorithmTester([LocalSearchAlgorithm(1), GreedyByPositionAlgorithm(2), GreedyByPositionAlgorithm(3), GreedyByPositionAlgorithm(4), GreedyByPositionAlgorithm(5), GreedyByPositionAlgorithm(6), GreedyByPositionAlgorithm(7), GreedyByPositionAlgorithm(8), GreedyByPositionAlgorithm(9), GreedyByPositionAlgorithm(10)])
 algoTester.runTest(2011)
 endTime = time.time()
 
-x = 0
-for a in algoTester.algorithms:
-	x = x + 1
-	a.printTeam()
+# x = 0
+# for a in algoTester.algorithms:
+# 	x = x + 1
+# 	a.printTeam()
 
-print endTime - startTime, "seconds to simulate this greedy draft"
+algoTester.algorithms[0].printTeam()
+
+print endTime - startTime, "seconds to simulate this draft"
